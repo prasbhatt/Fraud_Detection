d5f20fe401d82c23f8fdbeeca4e3fc29ea0fc732
PrasantaBhattacharjee <prasanta.bhattacharjee@gmail.com>
2024-12-08 15:12:03 +0530

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print ("machine learning model.. adding all ML related code here")
+
+
+def model1():
+    print ("model1")
+
+def model2():
+    print ("model2")
+
+def model3():
+    print ("model3")
